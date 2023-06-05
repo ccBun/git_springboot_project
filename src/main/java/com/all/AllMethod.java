5a62069ea7495b30e880bb2b19e3c7a82e29c82a
cen <2633957166@qq.com>
2023-06-05 23:30:46 +0800

    feature: add devM1

--- src/main/java/com/all/AllMethod.java
+++ src/main/java/com/all/AllMethod.java
@@ -1,4 +1,8 @@
 package com.all;
 
 public class AllMethod {
+
+    public void allM1() {
+        System.out.println("公有方法");
+    }
 }
---
+++ src/main/java/com/dev/DevMethod.java
@@ -0,0 +1,8 @@
+package com.dev;
+
+public class DevMethod {
+
+    public void devM1() {
+        System.out.println("dev exclusive");
+    }
+}
